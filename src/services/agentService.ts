744dddb167d50e28c7ca1ea77d5db86893005e1c
Federico Gobea <fgobea@chroma.agency>
2025-06-06 17:41:48 -0500

    FIXED now use form works as inteded

--- src/services/agentService.ts
+++ src/services/agentService.ts
@@ -5,7 +5,7 @@ export async function getIntegrations() {
 }
 export async function getAgents() : Promise<Agent[]> {
 
-    const agentsResp = await fetch('https://cd41-38-156-229-45.ngrok-free.app/api/agents',{method:'GET'})
+    const agentsResp = await fetch('https://quantum-agents-api.quantumcorp.com.mx/api/agents',{method:'GET'})
     const agents:ApiResult<ApiAgent> = await agentsResp.json()
     const  agentResult = agents.results;
     return agentResult.map(mapAgent)
